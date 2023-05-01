257091389de3fe4817b8eb90a79f84f5778c6e83
Felisa Wiley <35350290+felisawiley@users.noreply.github.com>
2023-05-01 12:25:35 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+#Store the email configuration and any other constants/settings.
+
+sender = ""
+recipient = ""
+password = ""
+url = "https://hacker-news.firebaseio.com/v0/jobstories.json"
+
+print("hacker news email digest config.py complete")
