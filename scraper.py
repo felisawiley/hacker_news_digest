212a9f6fec5ffae5e4d19691180994fa0beef251
Felisa Wiley <35350290+felisawiley@users.noreply.github.com>
2023-06-24 09:47:43 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,20 +1,23 @@
-#Handle the email-related tasks.
+#this modules fetches the necessary data from the Hacker News API, processes it, and then sends the email using the functionality defined in the other modules
 
-import requests
+import requests #used for making HTTP requests 
 from emailer import send_email
 from config import url
 
-
+#the following functions are required to obtain the job postings from HN - they collect and process the data 
+#fetches a snapshot from the Hacker News API with the latest story IDs
 def fetch_snapshot(url):
+    #attempts to get a response from the URL and parse it as JSON (needs to parse as JSON because data retrieved from HN API is in JSON)
     try:
         response = requests.get(url)
         response.raise_for_status()
-        return response.json()
+        return response.json() #converts from JSON to a list -- easier to work with in Python
+    #handles any potential exceptions
     except requests.exceptions.RequestException as e:
         print(f"Error fetching snapshot: {e}")
         return None
     
-
+#fetches a specific job positing using its ID, constructs the URL for the specific job posting and sends a GET request to the URL (to retrieve the data -- a type of HTTP)
 def fetch_story(story_id):
     story_url = f"https://hacker-news.firebaseio.com/v0/item/{story_id}.json"
     try:
@@ -25,18 +28,17 @@ def fetch_story(story_id):
         print(f"Error fetching story with ID {story_id}: {e}")
         return None
     
-
+#fetches a snapshot of job postings, iterates over the IDs to capture details of each job, appends to a list
 def job():
     snapshot = fetch_snapshot(url)
-    #print(f"Fetched snapshot: {snapshot}")
     if snapshot is not None:
         jobs = []
         for job_id in snapshot:
             job = fetch_story(job_id)
             if job is not None:
                 jobs.append(job)
-        #print(f"Job stories: {jobs}")
         print("calling send email...")
+        #calls send email using predefined functionality/ formatting
         send_email(jobs, url)
     else:
         print("Failed to fetch snapshot. Skipping email sending.")
